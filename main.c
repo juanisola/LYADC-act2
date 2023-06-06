696a5b566b8c63bf94c2f6f6e9f86939f9af9f2e
juan <104380950+juanisola@users.noreply.github.com>
2023-06-06 13:49:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,119 +1,57 @@
-// Inclusion de librerias
+// Inclusion de bibliotecas
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/adc.h"
-#include <math.h>
-
-// GPIO del 7 segmentos
-#define SEG_A   0
-#define SEG_B   1
-#define SEG_C   2
-#define SEG_D   3
-#define SEG_E   4
-#define SEG_F   5
-#define SEG_G   6
-#define SEG_DP  7
-#define DIG_1   8
-#define DIG_2   9
-#define DIG_3   10
-
-// GPIO del ADC
-#define ANALOG_GPIO 26
-// Canal del ADC
-#define ANALOG_CH   0
-
-// Mascara para GPIO de segmentos
-#define SEGMENTS  (1 << SEG_A) | (1 << SEG_B) | (1 << SEG_C) | (1 << SEG_D) | \  
-                  (1 << SEG_E) | (1 << SEG_F) | (1 << SEG_G) | (1 << SEG_DP)
-
-// Mascara para cada digito
-#define DIGIT_1   (1 << DIG_1)
-#define DIGIT_2   (1 << DIG_2)
-#define DIGIT_3   (1 << DIG_3)
-
-// Mascara para GPIO de digitos
-#define DIGITS    DIGIT_1 | DIGIT_2 | DIGIT_3
-
-// Tiempo de refresco para el siete segmentos
-#define SLEEP_MS 10
-
-// Coeficiente de temperatura del termistor
-#define BETA 3950
-
-/*
- * @brief Display temperature value in 7 segment display
- * @param temperature: value to display
- */
-void display_temp(float temperatura) {
-  // Array con valores de segmentos para cada numero (prende con cero el segmento)
-  const uint8_t digitos[10] = { 
-    ~0x3f, ~0x06, ~0x5b, ~0x4f, ~0x66, 
-    ~0x6d, ~0x7d, ~0x07, ~0x7f, ~0x6f 
-  };
-  // Variables para cada digito individualmente
-  uint8_t digit_1, digit_2, digit_3;
-  // Obtengo el valor de la decena
-  digit_1= temperatura/10; 
-  // Prendo el digito 1
-  gpio_put_masked(DIGITS, DIGIT_1);
-  // Dibujo el digito
-  gpio_put_masked(SEGMENTS, digitos[digit_1]);
-  // Espero
-  sleep_ms(SLEEP_MS);
-  // Obtengo el valor de la unidad
-  digit_2= temperatura-digit_1*10;
-  // Prendo el digito 2
-  gpio_put_masked(DIGITS, DIGIT_2);
-  // Dibujo el digito 2 con el punto
-  gpio_put_masked(SEGMENTS, digitos[digit_2] & ~(1 << SEG_DP));
-  // Espero
-  sleep_ms(SLEEP_MS);
-  // Obtengo el valor del primer decimal
-  digit_3= temperatura*10-digit_2*10-digit_1*100; 
-  // Prendo el digito 3
-  gpio_put_masked(DIGITS, DIGIT_3);
-  // Dibujo el digito
-  gpio_put_masked(SEGMENTS, digitos[digit_3]);
-  // Espero
-  sleep_ms(SLEEP_MS);
-}
+#include "hardware/i2c.h"
+#include "lcd_i2c.h"
+#include "bmp280.h"
 
 /*
  * @brief Programa principal
- */
+ */ 
 int main() {
-  // Variable para almacenar el resultado del ADC
-  uint16_t adc_reading = 0;
-  // Variable para guardar el valor de temperatura
-  float temperatura = 0.0;
-  // Inicializacion de UART
-  stdio_init_all();
-  // Inicializacion de GPIO
-  gpio_init_mask(SEGMENTS | DIGITS);
-  // Configuro GPIO como salida
-  gpio_set_dir_out_masked(SEGMENTS | DIGITS);
-  // Valores iniciales de GPIO cero
-  gpio_put_masked(SEGMENTS | DIGITS, 0x0);
-  // Inicializo ADC
-  adc_init();
-  // Inicializo GPIO26 como entrada analogica
-  adc_gpio_init(26);
-  // Selecciono canal analogico
-  adc_select_input(0);
-  // Enciendo todos los digitos
-  gpio_put_masked(DIGITS, 0x0);
+    // Habilito USB
+    stdio_init_all();
+    i2c_init(i2c0, 100 * 1000);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+    gpio_pull_up(4);
+    gpio_pull_up(5);
+    // Inicializacion del LCD
+    lcd_init();
+    // Inicializo BMP280
+    bmp280_init();
+    // Obtengo parametros de compensacion
+    struct bmp280_calib_param params;
+    bmp280_get_calib_params(&params);
+    
+    // Variables para temperatura y presion
+    int32_t raw_temperature;
+    int32_t raw_pressure;
+    // Variable para texto 
+    char str[16];
+    // Darle tiempo al BMP que se calibre
+    sleep_ms(250);
 
-  // Bucle infinito
-  while (true) {
-    // Leer NTC
-    adc_reading= adc_read();
-    // Calculo temperatura
-    float temperatura= 1/(log(1 / (4096. / adc_reading - 1)) / BETA + 1.0 / 298.15) - 273.15;
-    // Puede verificarse el valor antes de mostrar en el display con el siguiente printf
-    // printf("Temperatura: %.2f\n", temperatura);
-    // Muestro temperatura
-    display_temp(temperatura);
-    // Espero
-    sleep_ms(SLEEP_MS);
-  }
+    while(true) {
+        // Leo temperatura y presion
+        bmp280_read_raw(&raw_temperature, &raw_pressure);
+        // Calculo temperatura y presion
+        int32_t temperature = bmp280_convert_temp(raw_temperature, &params);
+        int32_t pressure = bmp280_convert_pressure(raw_pressure, raw_temperature, &params);
+        // Limpio LCD
+        lcd_clear();
+        // Armo string
+        sprintf(str, "P=%.3f kPa", pressure / 1000.f);
+        // Imprimo string en primer fila
+        lcd_string(str);
+        // Muevo a segunda fila
+        lcd_set_cursor(1, 0);
+        // Creo segundo string
+        sprintf(str, "T=%.2f C", temperature / 100.f);
+        // Imprimo string en segunda fila
+        lcd_string(str);
+        // Espero 500 ms
+        sleep_ms(500);
+    }
+    return 0;
 }
